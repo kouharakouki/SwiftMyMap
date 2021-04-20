e8cb4d3abebb7e7f84ff0819ef47c712a09e4429
koukikouhara <koki.kohara916@gmail.com>
2021-04-20 19:48:34 +0900

    Initial Commit

---
+++ SwiftMyMap/SwiftMyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMyMapApp.swift
+//  SwiftMyMap
+//
+//  Created by 鴻原航輝 on 2021/04/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
