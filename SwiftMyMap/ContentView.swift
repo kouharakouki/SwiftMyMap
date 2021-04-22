1e16a01224322e88b3783dab13135ce9fc4c2826
koukikouhara <koki.kohara916@gmail.com>
2021-04-22 19:58:43 +0900

    Finish 1 Lesson5-4

--- SwiftMyMap/ContentView.swift
+++ SwiftMyMap/ContentView.swift
@@ -6,11 +6,13 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     
     @State var inputText:String = ""
     @State var dispSearchKey:String = ""
+    @State var dispMapType:MKMapType = .standard
     
     var body: some View {
         VStack {
@@ -19,8 +21,35 @@ struct ContentView: View {
                         dispSearchKey = inputText
                         print("入力したキーワード：" + dispSearchKey)
             })
-                .padding()
-            MapView(searchKey: dispSearchKey)
+            .padding()
+            
+            ZStack(alignment: .bottomTrailing) {
+                MapView(searchKey: dispSearchKey, mapType: dispMapType)
+                
+                Button(action: {
+                    if dispMapType == .standard {
+                        dispMapType = .satellite
+                    } else if dispMapType == .satellite {
+                        dispMapType = .hybrid
+                    } else if dispMapType == .hybrid {
+                        dispMapType = .satelliteFlyover
+                    } else if dispMapType == .satelliteFlyover {
+                        dispMapType = .hybridFlyover
+                    } else if dispMapType == .hybridFlyover {
+                        dispMapType = .mutedStandard
+                    } else {
+                        dispMapType = .standard
+                    }
+                }) {
+                    Image(systemName: "map")
+                        .resizable()
+                        .frame(width: 35.0,
+                               height: 35.0,
+                               alignment: .leading)
+                }
+                .padding(.trailing, 20.0)
+                .padding(.bottom, 30.0)
+            }
         }
     }
 }
--- SwiftMyMap/MapView.swift
+++ SwiftMyMap/MapView.swift
@@ -12,6 +12,8 @@ struct MapView: UIViewRepresentable {
     
     let searchKey:String
     
+    let mapType: MKMapType
+    
     func makeUIView(context: Context) -> MKMapView {
         MKMapView()
     }
@@ -19,6 +21,8 @@ struct MapView: UIViewRepresentable {
     func updateUIView(_ uiView: MKMapView, context: Context) {
         
         print(searchKey)
+        
+        uiView.mapType = mapType
         
         let geocoder = CLGeocoder()
         
@@ -44,6 +48,6 @@ struct MapView: UIViewRepresentable {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView(searchKey: "東京タワー")
+        MapView(searchKey: "東京タワー", mapType: .standard)
     }
 }
